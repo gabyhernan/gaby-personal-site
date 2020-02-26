bbc7b083f9b9243933834ef263f01d61e94cd0c3
Gaby <gabyhernandezlebron@gmail.com>
2020-02-25 22:02:35 -0500

    setting up project template file

---
+++ src/templates/project.js
@@ -0,0 +1,4 @@
+import React from "react"
+import Layout from "../components/layout"
+import { graphql } from "gatsby"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
