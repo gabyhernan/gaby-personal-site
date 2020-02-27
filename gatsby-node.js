00de446b38ff946e373f660227979b1f57517363
Gaby <gabyhernandezlebron@gmail.com>
2020-02-26 19:28:51 -0500

    creates project page from contentful cms

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,7 +4,6 @@ module.exports.createPages = async ({ graphql, actions }) => {
   const { createPage } = actions
   // 1. Get Path to template
   const blogTemplate = path.resolve("./src/templates/blog.js")
-
   // 2. Get markdown data
   const res = await graphql(`
     query {
@@ -22,6 +21,37 @@ module.exports.createPages = async ({ graphql, actions }) => {
     createPage({
       component: blogTemplate,
       path: `/blog/${edge.node.slug}`,
+      context: {
+        // obj that contains data that can be passed to template
+        slug: edge.node.slug,
+      },
+    })
+  })
+}
+
+// Grabs Project Template
+module.exports.createPages = async ({ graphql, actions }) => {
+  const { createPage } = actions
+  // 1. Get Path to template
+  const projectTemplate = path.resolve("./src/templates/project.js")
+  // 2. Get markdown data
+  const res = await graphql(`
+    query {
+      allContentfulProject {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
+    }
+  `)
+
+  res.data.allContentfulProject.edges.forEach(edge => {
+    console.log("coming from contentful i think", edge)
+    createPage({
+      component: projectTemplate,
+      path: `/projects/${edge.node.slug}`,
       context: {
         // obj that contains data that can be passed to template
         slug: edge.node.slug,
--- src/templates/project.js
+++ src/templates/project.js
@@ -2,3 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Head from "../components/head"
+
+const Project = () => {
+  return (
+    <Layout>
+      <Head title="to fill with project title here" />
+      <h1> This will be my project Template </h1>
+    </Layout>
+  )
+}
+
+export default Project
