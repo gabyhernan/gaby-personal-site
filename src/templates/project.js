17af37a44bdf48c682fbeea6fe376d967a889050
Gaby <gabyhernandezlebron@gmail.com>
2020-02-29 11:18:13 -0500

    fixing image broken on contentful

--- src/templates/project.js
+++ src/templates/project.js
@@ -34,7 +34,7 @@ const Project = props => {
         <div className={`row ${projectStyle.row}`}>
           <div className="col-7">
             <h1> {props.data.contentfulProject.projectTitle} </h1>
-            <img src={props.data.contentfulProject.projectHeroImage.file.url} alt={props.data.contentfulProject.projectHeroImage.title} />
+            {/* <img src={props.data.contentfulProject.projectHeroImage.file.url} alt={props.data.contentfulProject.projectHeroImage.title} /> */}
             <p>{props.data.contentfulProject.projectDescription.childMarkdownRemark.excerpt}</p>
             <p> {props.data.contentfulProject.collaborationCredit}</p>
           </div>
